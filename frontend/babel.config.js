ad03f83a8cac68dd6ed11b8f44058fa47768fb6f
Igor Luciano de Lima Moreira <igor.moreira@ifsuldeminas.edu.br>
2020-10-12 14:26:13 -0300

    📖 DOC: Adiciona informações sobre o babel

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,11 @@
+/**
+ * Babel
+ * Responsável por converter (transpilar) o código  do React  para um código que o browser entenda
+ * Presets: São configurações utilizadas pelo babel para realizar alguma determinada ação
+ * - @babel/preset-env: Módulo especial que compilará o código compatível com o que o navegador suporta
+ * - @babel/preset-react: Processa o JSX e converte de forma que o navegador possa entender ele
+ */
+
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
 };
